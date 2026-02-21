418b6daa2d5968ccdb11da522c63453398127547
lfaga-lab <lfaga@abc.gob.ar>
2026-02-21 14:31:32 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // 1) Pegá acá la URL del WebApp de Apps Script (termina en /exec)
 window.APP_CONFIG = {
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEBAPP_EXEC_URL_HERE"
+  API_URL: "https://script.google.com/macros/s/AKfycbzlvpbtl9pvDi4uj9kkZIYoZlFNmSTfwNsZgwlL5zPSHoU1q_3eWSvDzV_NY1A6DCfh1g/exec"
 };
